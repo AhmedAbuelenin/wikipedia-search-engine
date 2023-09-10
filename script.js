83800f43369f10ab7b8da1685da5b7d590a16c0d
Ahmed Abuelenin <ahprogsolutions@gmail.com>
2023-09-10 13:00:59 +0300

    added debounce technique for searching to decrease load on server

--- script.js
+++ script.js
@@ -1,37 +1,43 @@
-let resultsContainer = document.getElementsByClassName("container")[0]
+let resultsContainer = document.getElementsByClassName('container')[0]
+const searchInput = document.getElementsByClassName('search__input')[0]
 
-const validateInput = (el) => {
-    if(el.value === ""){
-        resultsContainer.innerHTML = "<p>Type something in the above search input</p>"
-    }else{
-        generateResults(el.value, el)
-    }
-}
-
-const generateResults = (searchValue, inputField) => {
-    fetch(
-        "https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch="
-        + searchValue
-    )
+const generateResults = () => {
+  fetch(
+    'https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch=' +
+      searchInput.value
+  )
     .then(response => response.json())
     .then(data => {
-        let results = data.query.search
-        let numberOfResults = data.query.search.length
-        resultsContainer.innerHTML = ""
-        for(let i=0; i<numberOfResults; i++) {
-            let result = document.createElement("div")
-            result.classList.add("results")
-            result.innerHTML = `
+      let results = data.query.search
+      let numberOfResults = data.query.search.length
+      resultsContainer.innerHTML = ''
+      for (let i = 0; i < numberOfResults; i++) {
+        let result = document.createElement('div')
+        result.classList.add('results')
+        result.innerHTML = `
             <div>
                 <h3>${results[i].title}</h3>
                 <p>${results[i].snippet}</p>
             </div>
             <a href="https://en.wikipedia.org/?curid=${results[i].pageid}" target="_blank">Read More</a>
             `
-            resultsContainer.appendChild(result)
-        }
-        if(inputField.value === ""){
-            resultsContainer.innerHTML = "<p>Type something in the above search input</p>"
-        }
+        resultsContainer.appendChild(result)
+      }
     })
 }
+
+const debounce = (func, timeout = 500) => {
+  let timerId
+
+  return (...args) => {
+    clearTimeout(timerId)
+    timerId = setTimeout(() => {
+      searchInput.value
+        ? func.apply(this, args)
+        : (resultsContainer.innerHTML =
+            '<p>Type something in the above search input</p>')
+    }, timeout)
+  }
+}
+
+const validateInput = debounce(generateResults)
